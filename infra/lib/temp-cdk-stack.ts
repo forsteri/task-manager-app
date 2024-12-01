b187d56a4aadae2f609bf12d163628a3049da16a
forsteri <forza0720@gmail.com>
2024-12-01 15:01:42 +0000

    S3バケットのバージョニングを有効化

--- infra/lib/temp-cdk-stack.ts
+++ infra/lib/temp-cdk-stack.ts
@@ -8,7 +8,7 @@ export class TempCdkStack extends cdk.Stack {
 
     // S3バケットを作成
     new s3.Bucket(this, 'Haji-TempTestBucket', {
-      versioned: false, // バージョニングを無効化（テスト用に簡素化）
+      versioned: true, // バージョニング
       removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にバケットも削除
       autoDeleteObjects: true, // オブジェクトも一緒に削除
     });
